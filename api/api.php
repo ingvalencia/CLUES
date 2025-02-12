6e6b4a39cd5c07cbaee5b5653f18e1962a014c5a
ingvalecia <ing.gvalenci@gmail.com>
2025-02-12 15:13:25 -0600

    Se realizo ajuste referente a la tabla clues

--- api/api.php
+++ api/api.php
@@ -3,19 +3,19 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 $servername = "localhost";
-$username = "root"; // Si tienes contraseña, agrégala aquí
+$username = "root"; 
 $password = "";
 $database = "catalogo_clues";
 
-// Conectar a MySQL
+
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar conexión
+
 if ($conn->connect_error) {
     die(json_encode(["error" => "Conexión fallida: " . $conn->connect_error]));
 }
 
-// Consulta SQL para obtener el número de CLUES por entidad
+// obtengo el número de CLUES por entidad
 $sql = "SELECT ENTIDAD, COUNT(DISTINCT CLUES) AS total_clues FROM clues GROUP BY ENTIDAD";
 
 $result = $conn->query($sql);
@@ -31,9 +31,7 @@ if ($result->num_rows > 0) {
     exit;
 }
 
-// Cerrar conexión
 $conn->close();
 
-// Devolver JSON
 echo json_encode($data, JSON_UNESCAPED_UNICODE);
 ?>
